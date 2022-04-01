16c3381cbb786183d48b18a652aea83165c41713
Drew Monroe <dvmonroe6@gmail.com>
2022-04-01 13:45:48 -0600

    Fix lint from last commit

--- test/sanity/http/find_test.rb
+++ test/sanity/http/find_test.rb
@@ -6,6 +6,6 @@ describe Sanity::Http::Find do
   let(:klass) { Sanity::Http::Find }
 
   it do
-    assert_equal klass.new(id: 'test_123').id, 'test_123'
+    assert_equal klass.new(id: "test_123").id, "test_123"
   end
 end
