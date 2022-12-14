c5817b9c5b95a270b845484124307324c5c7aa43
Jarrett Lusso <jclusso@gmail.com>
2022-12-14 11:06:47 -0500

    Added handling for queries with boolean attribute value.

--- test/sanity/groq/filter_test.rb
+++ test/sanity/groq/filter_test.rb
@@ -8,6 +8,7 @@ describe Sanity::Groq::Filter do
   describe ".call" do
     it { assert_equal "_id == '123'", subject.call(_id: "123") }
     it { assert_equal "_id == '123' && _type == 'movie'", subject.call(_id: "123", _type: "movie") }
+    it { assert_equal "active == true", subject.call(active: true) }
 
     context "logical operators" do
       it { assert_equal "_id == '123' && (_type == 'movie' || url == 'www.bar.com')", subject.call(_id: "123", and: {or: {_type: "movie", url: "www.bar.com"}}) }
@@ -34,5 +35,6 @@ describe Sanity::Groq::Filter do
       it { assert_equal "title in [\"Aliens\", \"Interstellar\"]", subject.call(title: %i[Aliens Interstellar]) }
       it { assert_equal "text match 'wo*'", subject.call(text: {match: "wo*"}) }
     end
+
   end
 end
