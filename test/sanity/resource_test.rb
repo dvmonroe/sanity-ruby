e25a57b37a147c80234bd46de3821c239e84300e
dvmonroe <dvmonroe6@gmail.com>
2024-02-22 10:50:20 -0700

    (bug) Fix auto serialization with different response objects
    Sanity sends back different response objects based on the endpoint we're
    querying.
    
    Resolves #29

--- test/sanity/resource_test.rb
+++ test/sanity/resource_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 describe Sanity::Resource do
-  let(:klass) { Sanity::Resource }
+  let(:klass) { Class.new(Sanity::Resource) { attribute :id; attribute :name } }
   subject { klass.new }
 
   it { assert_respond_to klass, :attributes }
@@ -11,4 +11,29 @@ describe Sanity::Resource do
   it { assert_respond_to klass, :default_serializer }
 
   it { assert_respond_to subject, :attributes }
+
+  describe "auto serialization" do
+    subject {
+      Class.new(Sanity::Resource) do
+        attribute :id
+        attribute :name
+        auto_serialize
+      end
+    }
+
+    let(:result_data) { { "result" => [{ "id" => 1, "name" => "Test Result" }] } }
+    let(:documents_data) { { "documents" => [{ "id" => 2, "name" => "Test Document" }] } }
+
+    it "correctly serializes data with 'result' key" do
+      serialized_objects = subject.default_serializer.call(result_data)
+      assert_equal 1, serialized_objects.first.id
+      assert_equal "Test Result", serialized_objects.first.name
+    end
+
+    it "correctly serializes data with 'documents' key" do
+      serialized_objects = subject.default_serializer.call(documents_data)
+      assert_equal 2, serialized_objects.first.id
+      assert_equal "Test Document", serialized_objects.first.name
+    end
+  end
 end
